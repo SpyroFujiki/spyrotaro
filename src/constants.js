21746a99999dc3dd68536c31f1885fb003dfbea7
SpyroFujiki <giahung29092006@gmail.com>
2026-04-26 01:19:46 +0700

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 export const SOCIAL_LINKS = {
-    FACEBOOK: "https://www.facebook.com/spyrotarot",
-    MESSENGER: "https://m.me/spyrotarot"
+    FACEBOOK: "https://www.facebook.com/nguyen.gia.hung.625828",
+    MESSENGER: "https://m.me/nguyen.gia.hung.625828"
 };
 
 export const BRAND_INFO = {
